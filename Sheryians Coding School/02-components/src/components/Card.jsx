866a80143922063715afc5d12ef472c4e2c38938
Off-Aditya <official.aditya.singh.343@gmail.com>
2025-10-24 19:12:07 +0530

    components

---
+++ Sheryians Coding School/02-components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className="card">
+            <h1>Aditya Singh</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, aut.</p>
+        </div>
+    )
+}
+
+export default Card
