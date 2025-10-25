ffd735cf0ecfedbdf99f699dd7489645ea5f04fe
Off-Aditya <official.aditya.singh.343@gmail.com>
2025-10-25 19:17:03 +0530

    Cards Project

--- Sheryians Coding School/04-cards-project/src/App.jsx
+++ Sheryians Coding School/04-cards-project/src/App.jsx
@@ -1,42 +1,24 @@
 import React from 'react'
 import Card from './components/Card'
-import {Bookmark} from 'lucide-react'
+import jobOpenings from './components/data'
+
 
 const App = () => {
-  return (
-  <>
-    <div className="parent">
-      <div className="card">
-        
-        <div className="top">
-          <img src="https://images.seeklogo.com/logo-png/40/2/amazon-icon-logo-png_seeklogo-405254.png" alt="" />
-          <button>Save <Bookmark/> </button>
-        </div>
-
-        <div className="center">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX Designer</h2>
-          <div className='tag'>
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
-          </div>
-        </div>
-
-        <div className="bottom">
-          <div>
-            <div>
-              <h3>$120/hr</h3>
-              <p>Mumbai, India</p>
-            </div>
-          </div>
-          <button>Apply Now</button>
-        </div>
-
-      </div>
-    </div>
   
-  </>
-    
+  return (
+    <>
+      <div className="parent">
+        {/* <Card /> */}
+        {jobOpenings.map((item,idx)=>
+          <div key={idx}>
+            <Card brandLogo={item.brandLogo} companyName={item.companyName} datePosted={item.datePosted} post={item.post} tag1={item.tag1} tag2={item.tag2} pay={item.pay} location={item.location} />
+          </div>
+          
+        )}
+      </div>
+
+    </>
+
   )
 }
 
---
+++ Sheryians Coding School/04-cards-project/src/components/data.js
@@ -0,0 +1,104 @@
+const jobOpenings = [
+    {
+        brandLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png",
+        companyName: "Google",
+        datePosted: "5 days ago",
+        post: "Frontend Developer",
+        tag1: "Full Time",
+        tag2: "Mid Level",
+        pay: "$68/hr",
+        location: "Bangalore, India"
+    },
+    {
+        brandLogo: "https://images.seeklogo.com/logo-png/40/2/amazon-icon-logo-png_seeklogo-405254.png",
+        companyName: "Amazon",
+        datePosted: "2 weeks ago",
+        post: "Data Engineer",
+        tag1: "Full Time",
+        tag2: "Senior Level",
+        pay: "$75/hr",
+        location: "Hyderabad, India"
+    },
+    {
+        brandLogo: "https://images.seeklogo.com/logo-png/42/2/meta-icon-new-facebook-2021-logo-png_seeklogo-424014.png",
+        companyName: "Meta",
+        datePosted: "3 days ago",
+        post: "React Native Developer",
+        tag1: "Part Time",
+        tag2: "Junior Level",
+        pay: "$55/hr",
+        location: "Remote"
+    },
+    {
+        brandLogo: "https://i.pinimg.com/736x/60/6b/c0/606bc0717982547e555a514b479365a0.jpg",
+        companyName: "Apple",
+        datePosted: "1 week ago",
+        post: "iOS Engineer",
+        tag1: "Full Time",
+        tag2: "Mid Level",
+        pay: "$80/hr",
+        location: "Cupertino, USA"
+    },
+    {
+        brandLogo: "https://images.icon-icons.com/2699/PNG/512/netflix_logo_icon_170919.png",
+        companyName: "Netflix",
+        datePosted: "10 days ago",
+        post: "Machine Learning Engineer",
+        tag1: "Full Time",
+        tag2: "Senior Level",
+        pay: "$95/hr",
+        location: "Los Gatos, USA"
+    },
+    {
+        brandLogo: "https://static.vecteezy.com/system/resources/previews/027/127/473/non_2x/microsoft-logo-microsoft-icon-transparent-free-png.png",
+        companyName: "Microsoft",
+        datePosted: "4 weeks ago",
+        post: "Backend Developer (C#/.NET)",
+        tag1: "Full Time",
+        tag2: "Mid Level",
+        pay: "$70/hr",
+        location: "Seattle, USA"
+    },
+    {
+        brandLogo: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/openai-icon.png",
+        companyName: "OpenAI",
+        datePosted: "2 weeks ago",
+        post: "AI Research Intern",
+        tag1: "Part Time",
+        tag2: "Intern Level",
+        pay: "$45/hr",
+        location: "San Francisco, USA"
+    },
+    {
+        brandLogo: "https://images.seeklogo.com/logo-png/44/2/nvidia-logo-png_seeklogo-443363.png",
+        companyName: "NVIDIA",
+        datePosted: "10 weeks ago",
+        post: "Graphics Software Engineer",
+        tag1: "Full Time",
+        tag2: "Senior Level",
+        pay: "$85/hr",
+        location: "Santa Clara, USA"
+    },
+    {
+        brandLogo: "https://www.pngplay.com/wp-content/uploads/13/Tesla-Logo-PNG-HD-Quality.png",
+        companyName: "Tesla",
+        datePosted: "6 days ago",
+        post: "Embedded Systems Developer",
+        tag1: "Full Time",
+        tag2: "Mid Level",
+        pay: "$72/hr",
+        location: "Austin, USA"
+    },
+    {
+        brandLogo: "https://static.cdnlogo.com/logos/a/90/adobe.png",
+        companyName: "Adobe",
+        datePosted: "3 weeks ago",
+        post: "UI/UX Designer",
+        tag1: "Part Time",
+        tag2: "Junior Level",
+        pay: "$52/hr",
+        location: "Noida, India"
+    }
+];
+
+export default jobOpenings;
