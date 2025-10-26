f8b7de1b67aec459bdbf14de96c3be94d77ac2ce
Off-Aditya <official.aditya.singh.343@gmail.com>
2025-10-26 13:43:52 +0530

    Content

---
+++ Sheryians Coding School/07-ui-project/src/components/Section1/LeftContent.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const LeftContent = () => {
+    return (
+        <div className='h-full w-1/4 bg-amber-300'>
+            {/* <div>
+                <h1 className='font-bold text-[2.5rem]'>
+                    <p>Prospective</p> 
+                    <span className='bg-gray-50 rounded-full px-1.5'>customer</span> 
+                    <p>segmentation</p>
+                </h1>
+            </div>
+            <div>
+                <p>Depending on customer satisfaction and access to banking products, potential target audience can be divided into three groups</p>
+            </div> */}
+
+
+
+            
+        </div>
+    )
+}
+
+export default LeftContent
