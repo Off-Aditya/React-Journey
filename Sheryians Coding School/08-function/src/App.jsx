85550cf1bd5dbc2cbd911e4a2d0f0d080d74c71a
Off-Aditya <official.aditya.singh.343@gmail.com>
2025-10-27 14:30:34 +0530

    Function

--- Sheryians Coding School/08-function/src/App.jsx
+++ Sheryians Coding School/08-function/src/App.jsx
@@ -17,18 +17,54 @@ const App = () => {
     console.log(elem.target.value)
   }
 
+  function inputChanging(val) {
+    console.log(val);
+  }
+
+  const pageScrolling = (val) => {
+    // console.log(val);
+    if (val > 0) {
+      console.log('Downward Scrolling');
+    } else {
+      console.log('Upward Scrolling');
+    }
+  }
+
   return (
     <>
-      <h1>Hello, Neon</h1>
 
-      <button onMouseEnter={mouseHover} onClick={singleClicked} onDoubleClick={doubleClicked}>change user</button>
+      <div onWheel={(elem) => {
+        // console.log(elem.deltaY);
+        pageScrolling(elem.deltaY);
+      }}>
 
-      <button onClick={() => {
-        console.log('Button Clicked');
-      }}>Click Here</button>
+        <div className="page1"><h1>Hello, Neon</h1>
+
+          <button onMouseEnter={mouseHover} onClick={singleClicked} onDoubleClick={doubleClicked}>change user</button>
+
+          <button onClick={() => {
+            console.log('Button Clicked');
+          }}>Click Here</button>
 
 
-      <input type="text" placeholder='Enter Name' onChange={typing} />
+          <input type="text" placeholder='Enter Name' onChange={typing} />
+
+          <input type="text" placeholder='Again'
+            onChange={function (elem) {
+              inputChanging(elem.target.value)
+            }} />
+
+
+          <div>
+            <div
+              onMouseMove={(elem) => {
+                console.log(elem.clientX, elem.clientY)
+              }}
+              className="box"></div>
+          </div></div>
+        <div className="page2"></div>
+        <div className="page3"></div>
+      </div>
 
     </>
   )
