5262c23d54437f88b431f0db50cf0482a04c5fc5
Off-Aditya <official.aditya.singh.343@gmail.com>
2025-10-27 23:17:36 +0530

    second useState

--- Sheryians Coding School/09-useState/src/App.jsx
+++ Sheryians Coding School/09-useState/src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [num, setNum] = useState(0)
+
+  const increaseNum = () => {
+    setNum(num+1);
+  }
+
+  const decreaseNum = () => {
+    setNum(num-1);
+  }
+
+  const increaseFive = () => {
+    setNum(num+5);
+  }
+
+  const decreaseFive = () => {
+    setNum(num-5);
+  }
+
   return (
-    <div>App</div>
+    <>
+      <h1>{num}</h1>
+      <button onClick={increaseNum} >Increase</button>
+      <button onClick={decreaseNum} >Decrease</button>
+      <button onClick={increaseFive} >Increment by 5</button>
+      <button onClick={decreaseFive} >Decrement by 5</button>
+    </>
   )
 }
 
