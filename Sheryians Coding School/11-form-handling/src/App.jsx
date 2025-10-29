385c0cb83fcc98aa6ed7fdef1604a9e333d3f5f2
Off-Aditya <official.aditya.singh.343@gmail.com>
2025-10-29 17:52:55 +0530

    form handling

--- Sheryians Coding School/11-form-handling/src/App.jsx
+++ Sheryians Coding School/11-form-handling/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 const App = () => {
+
+  const submitHandler = (e) => {
+    e.preventDefault();  // use to not refreshing the page after form
+    console.log('Form Submitted !!');
+  }
   return (
-    <div>App</div>
+    <div>
+      {/* <form onSubmit={submitHandler}> */}
+      <form onSubmit={(e)=>{
+        submitHandler(e);
+      }}>
+        <input type="text" placeholder='Enter your name' />
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
 
